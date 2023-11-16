4957cd0c396451b220f54ae042618ccfd8308610
Daniel <danielson@college.harvard.edu>
2023-11-15 23:20:34 -0500

    chat bot

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 import { Client, Events, GatewayIntentBits } from "discord.js";
 import { config } from 'dotenv';
+import { OpenAI } from 'openai';
 import * as comms from './commands/commands.js';
 
 
@@ -8,7 +9,7 @@ config();
 console.log(process.env.SERVERID);
 
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds],
+    intents: ['Guilds', 'GuildMembers', 'GuildMessages', 'MessageContent', GatewayIntentBits.Guilds],
 });
 
 function readyDiscord() {
@@ -19,7 +20,48 @@ client.once(Events.ClientReady, readyDiscord);
 
 client.login(process.env.TOKEN)
 
-client.on(Events.InteractionCreate, handleInteraction);
+const IGNORE_PREFIX = '!';
+const CHANNELS = ['677374526069407752']
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_KEY 
+})
+
+client.on('messageCreate', async (message) => {
+    if (message.author.bot) return;
+    if (message.content.startsWith(IGNORE_PREFIX)) return;
+    if (!CHANNELS.includes(message.channelId) || !message.mentions.users.has(client.user.id)) return;
+
+    await message.channel.sendTyping();
+
+    const sendTypingInterval = setInterval(() => {
+        message.channel.sendTyping();
+    }, 5000);
+
+    const response = await openai.chat.completions
+        .create({
+            model: 'gpt-3.5-turbo',
+            messages: [
+                {
+                    // name: 
+                    role: 'system',
+                    content: 'something'
+                },
+                {
+                    // name:
+                    role: 'user',
+                    content: message.content,
+                }
+            ]
+        })
+        .catch((error) => console.error('OpenAI Error: \n', error));
+
+    clearInterval(sendTypingInterval);
+
+    message.reply(response.choices[0].message.content)
+})
+
+// client.on(Events.InteractionCreate, handleInteraction);
 
 async function handleInteraction(interaction) {
     if (!interaction.isCommand()) return;
